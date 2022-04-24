0e407c8d4412753bf80407116a71354208fb1dea
hajalalaina <rahajalalaina@yahoo.fr>
2022-04-24 13:22:20 +0300

    add routes/user

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let app = express();
 let bodyParser = require('body-parser');
 let assignment = require('./routes/assignments');
 let matiere = require('./routes/route-matieres');
+let users = require('./routes/users');
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
@@ -10,34 +11,37 @@ mongoose.Promise = global.Promise;
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
 const uri =
-
-'mongodb+srv://tpAngular:tpAngular@cluster0.hjrjl.mongodb.net/assignments?retryWrites=true&w=majority';
+  'mongodb+srv://tpAngular:tpAngular@cluster0.hjrjl.mongodb.net/assignments?retryWrites=true&w=majority';
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify:false
+  useFindAndModify: false,
 };
 
-mongoose.connect(uri, options)
-  .then(() => {
-    console.log("Connecté à la base MongoDB assignments dans le cloud !");
-    console.log("at URI = " + uri);
-    },
-    err => {
-      console.log('Erreur de connexion: ', err);
-    });
+mongoose.connect(uri, options).then(
+  () => {
+    console.log('Connecté à la base MongoDB assignments dans le cloud !');
+    console.log('at URI = ' + uri);
+  },
+  (err) => {
+    console.log('Erreur de connexion: ', err);
+  }
+);
 
 // Pour accepter les connexions cross-domain (CORS)
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
 // Pour les formulaires
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 let port = process.env.PORT || 8010;
@@ -45,22 +49,23 @@ let port = process.env.PORT || 8010;
 // les routes
 const prefix = '/api';
 
-app.route(prefix + '/assignments')
+app.use(prefix + '/users', users);
+
+app
+  .route(prefix + '/assignments')
   .get(assignment.getAssignments)
   .post(assignment.postAssignment)
   .put(assignment.updateAssignment);
 
-app.route(prefix + '/assignments/:id')
+app
+  .route(prefix + '/assignments/:id')
   .get(assignment.getAssignment)
   .delete(assignment.deleteAssignment);
 
-app.route(prefix + '/matieres')
-  .get(matiere.getMatieres);
+app.route(prefix + '/matieres').get(matiere.getMatieres);
 
 // On démarre le serveur
-app.listen(port, "0.0.0.0");
+app.listen(port, '0.0.0.0');
 console.log('Serveur démarré sur http://localhost:' + port);
 
 module.exports = app;
-
-
